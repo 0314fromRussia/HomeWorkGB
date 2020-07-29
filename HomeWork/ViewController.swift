4f748c9a6c02ce5143ea5d625d4e7981a6862012
Nikita <a2143319@yandex.ru>
2020-07-29 23:42:20 +0500

    Initial Commit

---
+++ HomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by Никита Дмитриев on 29.07.2020.
+//  Copyright © 2020 Никита Дмитриев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
